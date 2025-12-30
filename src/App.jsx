1861b42b842b064191cc4f59aefcd020972ac273
Robert Gjoshevski <robert.gjoshevski@outlook.com>
2025-12-30 19:49:44 +0100

    [efffects]

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,41 @@ import Features from './components/Features'
 import Services from './components/Services'
 import Portfolio from './components/Portfolio'
 import Footer from './components/Footer'
+import LiquidEther from './components/reactbits/LiquidEther'
 import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <main>
-        <Hero />
-        <Features />
-        <Services />
-        <Portfolio />
-      </main>
-      <Footer />
+      <div style={{ width: '100%', height: '100vh', position: 'fixed', top: 0, left: 0, zIndex: 0, pointerEvents: 'none' }}>
+        <LiquidEther
+          colors={['#5227FF', '#FF9FFC', '#B19EEF']}
+          mouseForce={20}
+          cursorSize={100}
+          isViscous={false}
+          viscous={30}
+          iterationsViscous={32}
+          iterationsPoisson={32}
+          resolution={0.5}
+          isBounce={false}
+          autoDemo={true}
+          autoSpeed={0.5}
+          autoIntensity={2.2}
+          takeoverDuration={0.25}
+          autoResumeDelay={3000}
+          autoRampDuration={0.6}
+        />
+      </div>
+      <div style={{ position: 'relative', zIndex: 1 }}>
+        <Header />
+        <main>
+          <Hero />
+          <Features />
+          <Services />
+          <Portfolio />
+        </main>
+        <Footer />
+      </div>
     </div>
   )
 }
--- src/components/reactbits/index.js
+++ src/components/reactbits/index.js
@@ -2,4 +2,6 @@ export { default as TiltedCard } from './TiltedCard'
 export { default as FadeIn } from './FadeIn'
 export { default as StaggerContainer } from './StaggerContainer'
 export { default as StaggerItem } from './StaggerItem'
+export { default as FloatingLines } from './FloatingLines'
+export { default as LiquidEther } from './LiquidEther'
 
